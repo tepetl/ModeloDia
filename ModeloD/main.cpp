8bb7f85eda657054c09fd3f857df07c03bfd5851
alfonso <alfonso@CGAlf>
2013-07-10 14:06:22 -0500

    Primera importación - esqueleto -

---
+++ ModeloD/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: alfonso
+ *
+ * Created on 10 de julio de 2013, 02:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
